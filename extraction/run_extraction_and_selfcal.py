3e91a8ba7b80513cd4f30a4524e679aecf4675d0
Gabriella Di Gennaro <g.digennaro@ira.inaf.it>
2026-03-05 09:47:58 +0100

    Update run_extraction_and_selfcal.py

--- extraction/run_extraction_and_selfcal.py
+++ extraction/run_extraction_and_selfcal.py
@@ -109,7 +109,7 @@ for i, cluster in enumerate(clusterlist):
       os.chdir(CLUSTERDIR)
       print (os.getcwd())
       
-      cmd  = 'python facetselfcal '
+      cmd  = 'facetselfcal '
       cmd += '-b %s.ds9.reg '%name
       cmd += '--auto '  
       cmd += '-i %s *dysco.sub.shift.avg.weights.ms.archive?'%name
